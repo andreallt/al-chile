9ad12fa8f793e144d8a4af11a0c7ec24eccb39f2
andreallata <andrea.alt92@gmail.com>
2021-06-29 17:02:50 -0400

    Sauce page style and toggle effect

--- src/components/Sauce.jsx
+++ src/components/Sauce.jsx
@@ -12,16 +12,17 @@ function toggle() {
 
   return(
     <article>
+    <Heat hotLevels={hotLevels} />
     <h2 style={{
         margin: "0",
         fontFamily: "Red Hat Text, sans-serif",
         textAlign: "center",
        }}>{sauce}</h2>
-    <Heat hotLevels={hotLevels} />
     <img onClick={toggle}
       src={image} alt={sauce}></img>
     
     {showMore === true ?(
+    <section className="info-box">
     <div className="info">
     <h3>Great on:</h3>
     <p>{pairing}</p>
@@ -29,8 +30,16 @@ function toggle() {
     <p>{shu}</p>
     <h3>Peppers:</h3>
     <p>{peppers}</p>
-    </div> ):(""
+    </div> 
+    </section>):(""
       )}
+      <hr style={{
+            color: "black",
+            backgroundColor: "lightgray",
+            width: "95vw",
+            alignContent: "center",
+            margin: "0",
+        }}/>  
     </article>
   )
 }
